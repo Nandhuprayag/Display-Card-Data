fbfd99ac5629d2ef0f09e3d21bac8fc523e353f2
Nandhuprayag <khokhonandhu@gmail.com>
2022-08-29 15:57:25 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+// import { CardList } from './CardList';
+import { Button } from "@mui/material";
 
-function App() {
-  return (
+const cardlist=
+  [{
+    select:"FREE",
+    price:"$0",
+    month:"/month",
+    name:"Single User",
+    storage:"5GB Storage",
+    projects:"Unlimited Public Projects",
+    access:"Community Access",
+    private:"Unlimited Private Projects",
+    support:"Dedicated Phone Support",
+    domain:"Free SubDomain",
+    reports:"Monthly status"
+  },
+  {
+    select:"Plus",
+    price:"$9",
+    month:"/month",
+    name:"5 User",
+    storage:"50GB Storage",
+    projects:"Unlimited Public Projects",
+    access:"Community Access",
+    private:"Unlimited Private Projects",
+    support:"Dedicated Phone Support",
+    domain:"Free SubDomain",
+    reports:"Monthly status"
+  
+  },
+  {
+    select:"PRO",
+    price:"$49",
+    month:"/month",
+    name:"Unlimited Users",
+    storage:"150GB Storage",
+    projects:"Unlimited Public Projects",
+    access:"Community Access",
+    private:"Unlimited Private Projects",
+    support:"Dedicated Phone Support",
+    domain:"Unlimited Free SubDomain",
+    reports:"Monthly status"
+  
+  }];
+
+
+function App() 
+{
+  const Items=cardlist;
+
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <nav>
+      <ul>
+        <li>
+          <Link to='/Card'>Cards</Link>
+        </li>
+      </ul>
+     </nav>
+ <Routes>
+  <Route path='/Card' element={<CardList Items={Items}/>} />
+ </Routes> */}
+  <div className='card-list'>
+  {Items.map((Item, index) =>(<ShowCard card={Item} key={index}/>))}
+  </div>
+
     </div>
   );
+}
+
+function ShowCard({card})
+{
+  return(
+    <section className="pricing py-5">
+    <div className="card-container">
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="card mb-5 mb-lg-0">
+            <div className="card-body">
+              <h5 className="card-title text-muted text-uppercase text-center">{card.select}</h5>
+              <h6 className="card-price text-center">{card.price}<span className='period'>{card.month}</span></h6>
+              <hr/>
+              <ul className="fa-ul">
+                <li><span className="fa-li"><i className="fas fa-check">✔️</i></span>{card.name}</li>
+                <li><span className="fa-li"><i className="fas fa-check">✔️</i></span>{card.storage}</li>
+                <li><span className="fa-li"><i className="fas fa-check">✔️</i></span>{card.projects}</li>
+                <li><span className="fa-li"><i className="fas fa-check">✔️</i></span>{card.access}</li>
+                <li className="text-muted"><span className="fa-li">❌<i className="fas fa-times"></i></span>{card.private}</li>
+                <li className="text-muted"><span className="fa-li">❌<i className="fas fa-times"></i></span>{card.support}</li>
+                <li className="text-muted"><span className="fa-li">❌<i className="fas fa-times"></i></span>{card.domain}
+                </li>
+                <li className="text-muted"><span className="fa-li">❌<i className="fas fa-times"></i></span>{card.reports}
+                  Reports</li>
+              </ul>
+              <div className="d-grid">
+              <Button  variant="contained">Button</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        
+      </div>
+    </div>
+  </section>
+  )
 }
 
 export default App;
